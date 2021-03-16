fc22ff1210080948a8d3e2fadd75028ee9f451a8
Adhvik Murarisetty <74809142+adhvik24@users.noreply.github.com>
2021-03-16 19:25:14 +0530

    Update assign2.py

--- ASSIGNMENT 2/codes/assign2.py
+++ ASSIGNMENT 2/codes/assign2.py
@@ -1,1 +1,13 @@
-
+import numpy as np  
+import matplotlib.pyplot as plt  
+  
+# data to be plotted 
+x = np.arange(0,2)  
+y =2*x 
+  
+# plotting 
+plt.ylabel("f(x)")  
+plt.xlabel("x")  
+plt.title("probability density function")
+plt.plot(x, y, color ="blue")  
+plt.show()

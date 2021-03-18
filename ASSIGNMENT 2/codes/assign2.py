809bd0d4c2ce841bf2088326c84de38c52b65011
Adhvik Murarisetty <74809142+adhvik24@users.noreply.github.com>
2021-03-18 23:04:53 +0530

    Update assign2.py

--- ASSIGNMENT 2/codes/assign2.py
+++ ASSIGNMENT 2/codes/assign2.py
@@ -1,7 +1,11 @@
 import numpy as np
-import scipy 
-import matplotlib.pyplot as plt 
-  
+from scipy.stats import uniform
+from matplotlib import pyplot as plt
+
+#if using termux
+import subprocess
+import shlex
+#end if  
 # data to be plotted 
 x = np.arange(0,2)  
 y =2*x 
@@ -12,6 +16,27 @@ plt.xlabel("x")
 plt.title("probability density function")
 plt.plot(x, y, color ="blue")  
 plt.show()
+
+
+def ecdf(a):
+    x, counts = np.unique(a, return_counts=True)
+    cusum = np.cumsum(counts)
+    return x, cusum / cusum[-1]
+
+U = uniform.rvs(size=100)
+V= U**0.5
+x, y = ecdf(V)
+x = np.insert(x, 0, x[0])
+y = np.insert(y, 0, 0.)
+plt.plot(x, y,'o',label='Simulation')
+plt.grid(True)
+x=np.linspace(0,1)
+plt.plot(x,x**2,label='Analysis')
+plt.title("CDF of random variable $X$")
+plt.xlabel("$x$")
+plt.ylabel("$F_X(x)$")
+plt.legend(loc='best')
+
 
 X = np.linspace(0,1,10001)
 probability = [ ]

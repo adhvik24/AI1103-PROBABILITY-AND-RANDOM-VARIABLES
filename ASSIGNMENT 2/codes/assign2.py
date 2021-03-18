7f1fc344127b9775b82da3bc32af696e2706d644
Adhvik Murarisetty <74809142+adhvik24@users.noreply.github.com>
2021-03-18 17:11:53 +0530

    Update assign2.py

--- ASSIGNMENT 2/codes/assign2.py
+++ ASSIGNMENT 2/codes/assign2.py
@@ -1,5 +1,6 @@
-import numpy as np  
-import matplotlib.pyplot as plt  
+import numpy as np
+import scipy 
+import matplotlib.pyplot as plt 
   
 # data to be plotted 
 x = np.arange(0,2)  
@@ -11,3 +12,15 @@ plt.xlabel("x")
 plt.title("probability density function")
 plt.plot(x, y, color ="blue")  
 plt.show()
+
+X = np.linspace(0,1,10001)
+probability = [ ]
+for i in range(10001): 
+  probability.append(2*X[i]/10001)
+count=0
+for i in range(10001):
+  Random_variable = np.random.choice(X, p = probability)
+  if Random_variable<0.5:
+        count +=1
+print("simulated probability P(X<0.5) is : ",count/10001)
+print("Theoritical probability P(X<0.5) is : 0.25")
